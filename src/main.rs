73158c2a36efe53119a198f77213beca9bb74adc
teschiopol <paolo.concolato@eniac.it>
2022-07-06 09:58:33 +0200

    feat(main): simple read input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io; // I/O library come from standard library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // add mut because by default variables are immutable
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line"); // Manage 'Err' result case
+
+    println!("You guessed: {guess}");
+    /*
+    * println!("x = {} and y = {}", x, y);
+    * Another solution for placeholder print
+    */
 }
+
+// cargo run
+// cargo test
